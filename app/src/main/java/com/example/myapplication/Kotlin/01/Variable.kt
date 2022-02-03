20d26f061001e2d3aa7e26074ce20d9af73a286d
JIHO KIM <kimjiho1125a@naver.com>
2022-02-03 21:57:23 +0900

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/Kotlin/01/Variable.kt
@@ -0,0 +1,23 @@
+package com.example.myapplication.Kotlin.`01`
+
+var num = 10
+var hello = "hello"
+var point = 3.4
+
+val fix = 20
+
+fun main(args:Array<String>){
+    println(num)
+    println(hello)
+    println(point)
+    println(fix)
+
+    num = 100
+    hello = "Good Bye"
+    point = 10.4
+
+    println(num)
+    println(hello)
+    println(point)
+
+}
