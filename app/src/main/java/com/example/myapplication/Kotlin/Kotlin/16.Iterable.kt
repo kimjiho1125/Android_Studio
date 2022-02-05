480927cf4948b5369bf0e665669cc759f967b9a4
JIHO KIM <kimjiho1125a@naver.com>
2022-02-05 21:57:46 +0900

    16. Iterable

--- app/src/main/java/com/example/myapplication/Kotlin/Kotlin/16.Iterable.kt
+++ app/src/main/java/com/example/myapplication/Kotlin/Kotlin/16.Iterable.kt
@@ -17,9 +17,58 @@ fun main(array: Array<String>) {
     println()
 
     // 반복하는 방법 (2)
-    for ((index,item) in a.withIndex()) {
+    for ((index, item) in a.withIndex()) {
         println("index : " + index + " value : " + item)
         // 문자열 + Int(정수) = 문자열
         // 문자열 + 아무거나 = 문자열
+    }
+
+    println()
+    // 반복하는 방법 (3)
+    a.forEach {
+        println(it)
+    }
+
+    // 반복하는 방법 (4)
+    a.forEach { item ->
+        println(item)
+    }
+
+    // 반복하는 방법 (5)
+    a.forEachIndexed { index, item ->
+        println("index : " + index + " value : " + item)
+    }
+
+    // 반복하는 방법 (6)
+    for (i in 0 until a.size) {
+        // until은 마지막을 포함 하지 않는다
+        // 0부터 8까지
+        println(a.get(i))
+    }
+    println()
+
+    // 반복하는 방법 (7)
+    for (i in 0 until a.size step (2)) {
+        println(a.get(i))
+    }
+    println()
+
+    // 반복하는 방법 (8)
+    for (i in a.size - 1 downTo (0)) {
+        //인덱스 8부터 0까지 반복
+        println(a.get(i))
+    }
+    println()
+
+    // 반복하는 방법 (9)
+    for (i in a.size - 1 downTo (0) step (2)) {
+        println(a.get(i))
+    }
+    println()
+
+    // 반복하는 방법 (10)
+    for (i in 0..10) {
+        // .. -> 마지막을 포함한다
+        println(i)
     }
 }
