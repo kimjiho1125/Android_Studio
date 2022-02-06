29e18aade6eba7c10463ffa315778c99c86ee39d
JIHO KIM <kimjiho1125a@naver.com>
2022-02-06 22:00:48 +0900

    18. Class -1

---
+++ app/src/main/java/com/example/myapplication/Kotlin/Kotlin/18.Class.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication.Kotlin.Kotlin
+
+// 18. Class
+// OOP -> Object Oriented Programming (객체지향 프로그래밍)
+
+// 객체를 만드는 방법
+// - 설명서가 있어야 한다
+
+fun main(array: Array<String>) {
+
+}
