5e2d58daac5c75fbee5c92a5faedb0a96aa8db60
JIHO KIM <kimjiho1125a@naver.com>
2022-02-06 22:09:51 +0900

    18. Class -2

--- app/src/main/java/com/example/myapplication/Kotlin/Kotlin/18.Class.kt
+++ app/src/main/java/com/example/myapplication/Kotlin/Kotlin/18.Class.kt
@@ -7,5 +7,31 @@ package com.example.myapplication.Kotlin.Kotlin
 // - 설명서가 있어야 한다
 
 fun main(array: Array<String>) {
+    // 클래스를 통해서 객체를 만드는 방법
+    // -> 인스턴스화 -> 인스턴스(객체)
+    Car("v8 engine", "big")
 
+    val bigCar = Car("v8 engine", "big")
+
+    // 우리가 만든 클래스는 자료형이 된다
+    var bigCar1 : Car = Car("v8 engine", "big")
+
+    var superCar : SuperCar = SuperCar("good engine", "big", "white")
+}
+
+// 클래스 만드는 방법 (1)
+class Car(var engine : String, var body: String) {
+
+}
+// 클래스 만드는 방법 (2)
+class SuperCar{
+    var engine : String
+    var body : String
+    var door : String
+
+    constructor(engine: String, body: String, door: String){
+        this.engine = engine
+        this.body = body
+        this. door = door
+    }
 }
