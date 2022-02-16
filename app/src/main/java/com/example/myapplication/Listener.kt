9aa64a31d6265ef5b70719e8e9c493e0f3372ba4
JIHO KIM <kimjiho1125a@naver.com>
2022-02-16 17:32:34 +0900

    xml 가져오기, 익명함수 실습

--- app/src/main/java/com/example/myapplication/Listener.kt
+++ app/src/main/java/com/example/myapplication/Listener.kt
@@ -2,6 +2,8 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_listener.*
 
@@ -17,6 +19,26 @@ class Listener : AppCompatActivity() {
         // 뷰를 activity로 가져오는 방법(2)
         // - xml을 import해서 가져온다다
 //       hello
+
+        // 익명함수1(축약) -> 람다 표현식
+        hello.setOnClickListener {
+            Log.d("Click","Click!!")
+        }
+
+        // 익명함수2 -> 위의 함수와 완전히 동일함
+        hello.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(v: View?) {
+                Log.d("Click","Click!!")
+            }
+        })
+
+        // 익명함수3
+        val click = object : View.OnClickListener{
+            override fun onClick(v: View?) {
+                Log.d("Click","Click!!")
+            }
+        }
+        hello.setOnClickListener(click)
 
     }
 }
