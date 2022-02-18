0b7b918bfc6b04809fa6b26fda95e79641b6d29c
JIHO KIM <kimjiho1125a@naver.com>
2022-02-18 18:00:33 +0900

    Intent 실습(3)

--- app/src/main/java/com/example/myapplication/Intent1.kt
+++ app/src/main/java/com/example/myapplication/Intent1.kt
@@ -3,7 +3,9 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_intent.*
+import kotlinx.android.synthetic.main.activity_intent2.*
 
 class Intent1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,9 +24,21 @@ class Intent1 : AppCompatActivity() {
             // Apply ->
             intent2.apply {
                 this.putExtra("number1",1)
-                this.putExtra("number2",2)
+                this.putExtra("number2",1)
             }
-            startActivity(intent2)
+            startActivityForResult(intent2,200)
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        if(requestCode == 200) {
+            Log.d("number","" + requestCode)
+            Log.d("number","" + resultCode)
+            var result = data?.getIntExtra("result",0)
+            Log.d("number","" + result)
+        }
+
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
--- app/src/main/java/com/example/myapplication/Intent2.kt
+++ app/src/main/java/com/example/myapplication/Intent2.kt
@@ -1,19 +1,33 @@
 package com.example.myapplication
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_intent2.*
 
 class Intent2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intent2)
 
-        var number1 = intent.getIntExtra("number1",0)
-        var number2 = intent.getIntExtra("number2",0)
+        result.setOnClickListener {
+            var number1 = intent.getIntExtra("number1",0)
+            var number2 = intent.getIntExtra("number2",0)
 
-        Log.d("number",number1.toString())
-        Log.d("number",number2.toString())
+            Log.d("number",number1.toString())
+            Log.d("number",number2.toString())
+
+            val result = number1 + number2
+            val resultIntent = Intent()
+            resultIntent.putExtra("result",result)
+
+            setResult(Activity.RESULT_OK,resultIntent)
+            finish() // -> Activity 종료
+            // Intent2  -> 종료
+            // Intent1                Intent1
+        }
 
     }
 }
