c26d70d3687040dbb9d344113f6c7fc5101fecac
JIHO KIM <kimjiho1125a@naver.com>
2022-02-19 21:10:21 +0900

    과제 인터넷 페이지 열기

---
+++ app/src/main/java/com/example/myapplication/OpenInternet.kt
@@ -0,0 +1,38 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_open_internet.*
+import java.util.*
+
+class OpenInternet : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_open_internet)
+
+        open.setOnClickListener {
+            var address = address_edit_text.text.toString()
+            var intent = Intent(Intent.ACTION_VIEW, Uri.parse(address))
+            startActivity(intent)
+        }
+
+        address_edit_text.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                Log.d("edit","beforeTextChanged : " + s)
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                Log.d("edit","onTextChanged : " + s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                Log.d("edit","afterTextChanged : " + s)
+            }
+        })
+    }
+}
