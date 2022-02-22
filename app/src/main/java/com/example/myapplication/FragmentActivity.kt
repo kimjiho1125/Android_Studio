248d6c7c6f1f0eb9d329733233c185f4f3c789e6
JIHO KIM <kimjiho1125a@naver.com>
2022-02-22 20:08:44 +0900

    Fragment 실습 (4)

--- app/src/main/java/com/example/myapplication/FragmentActivity.kt
+++ app/src/main/java/com/example/myapplication/FragmentActivity.kt
@@ -6,7 +6,12 @@ import android.util.Log
 import androidx.fragment.app.FragmentManager
 import kotlinx.android.synthetic.main.activity_fragment.*
 
-class FragmentActivity : AppCompatActivity() {
+class FragmentActivity : AppCompatActivity(), FragmentOne.OnDataPassListener {
+
+    override fun onDataPass(data: String?) {
+        Log.d("pass",""+data)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fragment)
