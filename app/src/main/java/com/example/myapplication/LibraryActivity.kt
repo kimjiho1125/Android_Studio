f38c55440ee4cf76058fb5caa25cc421994c89b4
JIHO KIM <kimjiho1125a@naver.com>
2022-02-25 20:27:20 +0900

    Library 실습

---
+++ app/src/main/java/com/example/myapplication/LibraryActivity.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_library.*
+
+class LibraryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_library)
+
+        Glide.with(this@LibraryActivity)
+            .load("https://i.ytimg.com/vi/sTCtOFG_wCM/maxresdefault.jpg")
+            .centerCrop()
+            .into(glide)
+
+    }
+}
