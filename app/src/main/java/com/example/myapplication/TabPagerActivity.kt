28ac356e179479ee1b6d8fc93bd33f020d79c20b
JIHO KIM <kimjiho1125a@naver.com>
2022-03-11 20:04:24 +0900

    TabLayout, Pager 실습(1)

---
+++ app/src/main/java/com/example/myapplication/TabPagerActivity.kt
@@ -0,0 +1,45 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import kotlinx.android.synthetic.main.activity_tab_pager.*
+
+class TabPagerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tab_pager)
+
+        tab_layout.addTab(tab_layout.newTab().setText("ONE"))
+        tab_layout.addTab(tab_layout.newTab().setText("TWO"))
+        tab_layout.addTab(tab_layout.newTab().setText("THREE"))
+
+    }
+}
+
+class PagerAdapter(
+    fragmentManager: FragmentManager,
+    val tabCount: Int
+): FragmentStatePagerAdapter(fragmentManager) {
+    override fun getCount(): Int {
+        return tabCount
+    }
+
+    override fun getItem(position: Int): Fragment {
+        when(position) {
+            0-> {
+                return Fragment1()
+            }
+            1-> {
+                return Fragment2()
+            }
+            2-> {
+                return Fragment3()
+            }
+            else -> return Fragment1()
+        }
+    }
+}
