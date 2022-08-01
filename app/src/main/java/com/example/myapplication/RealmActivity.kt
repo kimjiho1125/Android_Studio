b252727512ad382732ecd238ba23e10d60c3386f
kimjiho1125 <kimjiho1125a@naver.com>
2022-08-01 20:11:41 +0900

    Reaml 실습

---
+++ app/src/main/java/com/example/myapplication/RealmActivity.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.realm.Realm
+import io.realm.RealmConfiguration
+
+class RealmActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_realm)
+
+        Realm.init(this@RealmActivity)
+        val config : RealmConfiguration = RealmConfiguration.Builder()
+            .deleteRealmIfMigrationNeeded()
+            .build()
+        Realm.setDefaultConfiguration(config)
+        val realm = Realm.getDefaultInstance()
+    }
+}
