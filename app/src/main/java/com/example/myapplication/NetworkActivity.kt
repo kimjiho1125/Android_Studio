1516f6e6e27256ac9e08003b921c447a81d67354
kimjiho1125 <kimjiho1125a@naver.com>
2022-08-02 20:34:29 +0900

    Network 실습 (1)

---
+++ app/src/main/java/com/example/myapplication/NetworkActivity.kt
@@ -0,0 +1,36 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+
+class NetworkActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_network)
+
+
+        val urlString : String = "http://mellowcode.org/json/students/"
+        val url = URL(urlString)
+        val connection : HttpURLConnection = url.openConnection() as HttpURLConnection
+
+        connection.requestMethod = "GET"
+        connection.setRequestProperty("Content-Type", "application/json")
+
+        var buffer = ""
+        if(connection.responseCode == HttpURLConnection.HTTP_OK) {
+            Log.d("connn", "inputstream : " + connection.inputStream)
+            val reader = BufferedReader(
+                InputStreamReader(
+                    connection.inputStream,
+                    "UTF-8"
+                )
+            )
+            buffer = reader.readLine()
+        }
+    }
+}
