ca12ecc2d998dd2e9c80f1c534d8aa6bec7108d1
kimjiho1125 <kimjiho1125a@naver.com>
2022-08-08 17:51:33 +0900

    인스타그램 (1)

---
+++ app/src/main/java/com/example/myapplication/EmailSignupActivity.kt
@@ -0,0 +1,44 @@
+package com.example.myapplication
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class EmailSignupActivity : AppCompatActivity() {
+
+    lateinit var usernameView : EditText
+    lateinit var userPassword1View : EditText
+    lateinit var userPassword2View : EditText
+    lateinit var register : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_email_signup)
+
+        initView(this@EmailSignupActivity)
+    }
+
+    fun initView(activity : Activity)
+    {
+        usernameView = activity.findViewById(R.id.username_inputbox)
+        userPassword1View = activity.findViewById(R.id.password1_inputbox)
+        userPassword2View = activity.findViewById(R.id.password2_inputbox)
+    }
+
+    fun getUserName() : String {
+        return usernameView.text.toString()
+    }
+
+    fun getUserPassword1() : String {
+        return userPassword1View.text.toString()
+    }
+
+    fun getUserPassword2() : String {
+        return userPassword2View.text.toString()
+    }
+
+
+
+}
