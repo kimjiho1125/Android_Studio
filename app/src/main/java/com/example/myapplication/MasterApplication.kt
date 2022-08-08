cbe5f1af22ec7adebc8ba487637cc9d9ce2cb839
kimjiho1125 <kimjiho1125a@naver.com>
2022-08-08 20:26:36 +0900

    인스타그램 (2)

--- app/src/main/java/com/example/myapplication/MasterApplication.kt
+++ app/src/main/java/com/example/myapplication/MasterApplication.kt
@@ -1,11 +1,46 @@
 package com.example.myapplication
 
 import android.app.Application
+import com.facebook.stetho.okhttp3.StethoInterceptor
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MasterApplication : Application() {
+
+    lateinit var service : RetrofitService
+
     override fun onCreate() {
         super.onCreate()
 
+
+    }
+
+    fun creteRetrofit(){
+        val header = Interceptor {
+            val original = it.request()
+            val request = original.newBuilder()
+                .header("Authorization", "")
+                .build()
+            it.proceed(request)
+        }
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(header)
+            .addNetworkInterceptor(StethoInterceptor())
+            .build()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("http://mellowcode.org/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+
+        service = retrofit.create(RetrofitService::class.java)
+    }
+
+    fun checkIsLogin() : Boolean {
 
     }
 }
