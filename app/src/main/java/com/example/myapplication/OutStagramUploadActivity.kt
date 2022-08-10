6f3022e23a3c88358342e1f607da586b1abed83f
kimjiho1125 <kimjiho1125a@naver.com>
2022-08-10 22:52:59 +0900

    인스타그램 (9)

--- app/src/main/java/com/example/myapplication/OutStagramUploadActivity.kt
+++ app/src/main/java/com/example/myapplication/OutStagramUploadActivity.kt
@@ -1,11 +1,54 @@
 package com.example.myapplication
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.EditText
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class OutStagramUploadActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_out_stagram_upload)
+
+        val view_pictures = findViewById<TextView>(R.id.view_pictures)
+        val upload = findViewById<TextView>(R.id.upload2)
+
+        view_pictures.setOnClickListener {
+            getPicture()
+        }
+//        upload.setOnClickListener {
+//
+//        }
+    }
+
+    fun getPicture(){
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        intent.setType("image/*")
+        startActivityForResult(intent,1000)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == 1000) {
+            val uri : Uri = data!!.data!!
+            val a = getImageFilePath(uri)
+            Log.d("pathh","path : " + a)
+        }
+    }
+
+    fun getImageFilePath(contentUri : Uri) : String {
+        var columnIndex = 0
+        val projection = arrayOf(MediaStore.Images.Media.DATA)
+        val cursor = contentResolver.query(contentUri, projection, null, null, null)
+        if(cursor!!.moveToFirst()){
+            columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+        }
+        return cursor.getString(columnIndex)
     }
 }
